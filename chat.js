a75a58545575947371f7f8d763a1e6725208b24a
tucci1994 <82176681+tucci1994@users.noreply.github.com>
2023-06-16 09:15:20 +0200

    push

--- chat.js
+++ chat.js
@@ -1,1 +1,45 @@
 console.log("ciao");
+function Chat() {
+  var isChatOpen = false;
+
+  function toggleChat() {
+    isChatOpen = !isChatOpen;
+    updateChat();
+  }
+
+  function updateChat() {
+    var chatContainer = document.querySelector(".chat-container");
+    var chatCircle = document.querySelector(".chat-circle");
+    var chatBox = document.querySelector(".chat-box");
+
+    if (isChatOpen) {
+      chatCircle.classList.add("open");
+      chatBox.style.display = "block";
+    } else {
+      chatCircle.classList.remove("open");
+      chatBox.style.display = "none";
+    }
+  }
+
+  function createChatElement(className, content) {
+    var element = document.createElement("div");
+    element.className = className;
+    element.innerHTML = content;
+    return element;
+  }
+
+  function initializeChatGpt() {
+    var chatContainer = createChatElement("chat-container", "");
+    var chatCircle = createChatElement("chat-circle", "Chat");
+    var chatBox = createChatElement("chat-box", "");
+
+    chatCircle.addEventListener("click", toggleChat);
+
+    chatContainer.appendChild(chatCircle);
+    chatContainer.appendChild(chatBox);
+
+    document.body.appendChild(chatContainer);
+  }
+
+  initializeChatGpt();
+}
